2eaa408661c9f906b74fbb536f44701216f6db07
Ritika-soni <ritikasoni73@gmail.com>
2020-08-14 12:36:27 -0700

    Updates

---
+++ precache-manifest.8552d793e59caa2ede200e935c643dcf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d529f2a88f880a47a1f11dee09d4e9c",
+    "url": "/PanorbitAssignment/index.html"
+  },
+  {
+    "revision": "611d84562a9ea8303b81",
+    "url": "/PanorbitAssignment/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0d1b633e1ec8a328ff02",
+    "url": "/PanorbitAssignment/static/css/main.4dec4972.chunk.css"
+  },
+  {
+    "revision": "611d84562a9ea8303b81",
+    "url": "/PanorbitAssignment/static/js/2.f3d40bf5.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/PanorbitAssignment/static/js/2.f3d40bf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d1b633e1ec8a328ff02",
+    "url": "/PanorbitAssignment/static/js/main.5c7e4b65.chunk.js"
+  },
+  {
+    "revision": "43fda944728021ec3338",
+    "url": "/PanorbitAssignment/static/js/runtime-main.a2ba1635.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/PanorbitAssignment/static/media/SEGA.12a3db8b.woff"
+  }
+]);
